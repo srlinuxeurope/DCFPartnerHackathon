4e945aef1b53603202454671f31ffa6215b2f6df
sesantos <sergioliveirasantos@gmail.com>
2023-09-23 13:51:04 +0100

    added-NDK-Telemtry-lab

---
+++ srl-telemetry-ndk-lab/configs/fabric/leaf1_agent_install.sh
@@ -0,0 +1,10 @@
+sudo yum localinstall --disablerepo=* /home/admin/srl-ndk-git_0.1.0_linux_amd64.rpm -y
+sleep 2
+sudo sed -i "s/failure-threshold: 10/failure-action: wait=1/g" /etc/opt/srlinux/appmgr/ndk-git.yml
+sr_cli  "/ tools system app-management application app_mgr reload"
+sleep 5
+sr_cli --candidate-mode --commit-at-end < /home/admin/agent_config_leaf1.txt
+sr_cli --candidate-mode --commit-at-end '/ git-client branch '$GROUP'-branch'
+#Install Neofetch
+sudo ip netns exec srbase-mgmt dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+sudo ip netns exec srbase-mgmt dnf -y install neofetch

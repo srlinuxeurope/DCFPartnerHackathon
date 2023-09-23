ce25a8a5e46ef59066f662fbb2745803f0454822
sesantos <sergioliveirasantos@gmail.com>
2023-09-23 16:08:46 +0100

    fix git token access

--- srl-telemetry-ndk-lab/configs/fabric/leaf1_agent_install.sh
+++ srl-telemetry-ndk-lab/configs/fabric/leaf1_agent_install.sh
@@ -3,8 +3,18 @@ sleep 2
 sudo sed -i "s/failure-threshold: 10/failure-action: wait=1/g" /etc/opt/srlinux/appmgr/ndk-git.yml
 sr_cli  "/ tools system app-management application app_mgr reload"
 sleep 5
-sr_cli --candidate-mode --commit-at-end < /home/admin/agent_config_leaf1.txt
+sr_cli --candidate-mode < /home/admin/agent_config_leaf1.txt
+sr_cli --candidate-mode < /home/admin/git_token_setup.txt
 sr_cli --candidate-mode --commit-at-end '/ git-client branch '$GROUP'-branch'
+
+#while [[ ! $(sr_cli 'info from state / git-client') ]];
+#do
+#    sudo pkill ndk-git
+#    sleep 2
+#done
+
+
 #Install Neofetch
 sudo ip netns exec srbase-mgmt dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 sudo ip netns exec srbase-mgmt dnf -y install neofetch
+

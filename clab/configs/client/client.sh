c81d4029b1e451e228362ae34871c5b6dfd22910
srlinuxeurope <116505283+srlinuxeurope@users.noreply.github.com>
2025-09-22 12:21:16 +0100

    Remove user "user" and reference "nokia" user

--- clab/configs/client/client.sh
+++ clab/configs/client/client.sh
@@ -5,14 +5,16 @@
 
 ifup -a
 
-mkdir -p /home/user/.ssh
-touch /home/user/.ssh/authorized_keys
-chmod 600 /home/user/.ssh/authorized_keys
-cat /tmp/authorized_keys > /home/user/.ssh/authorized_keys
-chown -R user:user /home/user/.ssh
+adduser --disabled-password -s /bin/bash nokia
+cp /home/admin/.bash* /home/nokia
+mkdir -p /home/nokia/.ssh
+touch /home/nokia/.ssh/authorized_keys
+chmod 600 /home/nokia/.ssh/authorized_keys
+cat /tmp/authorized_keys > /home/nokia/.ssh/authorized_keys
+chown -R nokia:nokia /home/nokia/.ssh
 
-echo "user ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-echo "user:$USER_PASSWORD" | chpasswd
+echo "nokia ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+echo "nokia:$USER_PASSWORD" | sudo chpasswd
 
 # Start iperf3 server
 iperf3 -s -p 5201 -D 
